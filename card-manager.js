88e925979e76eca2b53213e3145251196c96e878
Dangerdangerau <75510060+Dangerdangerau@users.noreply.github.com>
2025-07-17 19:18:48 +0930

    Create card-manager.js

---
+++ card-manager.js
@@ -0,0 +1,7 @@
+import './cards/tabs_card.js';
+
+// Optional: Log for debugging
+console.info(
+  '%c Cards Galore is loaded!',
+  'color: white; background: #3498db; font-weight: bold; padding: 2px 4px; border-radius: 2px'
+);
